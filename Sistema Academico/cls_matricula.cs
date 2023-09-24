8359a43e719cff306c61b0f2d20bebd6e172178a
José David F <fontalvomejiajosedavid54@gmail.com>
2023-09-23 19:20:52 -0500

    Modulo de Matriculas

---
+++ Sistema Academico/cls_matricula.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Web;
+
+namespace Sistema_Academico
+{
+    public class cls_matricula
+    {
+        private string str_mensaje;
+        private string str_idestudiante;
+        private DateTime dt_fecha;
+        private float flt_subtotal;
+        private float flt_iva;
+        private float flt_total;
+
+
+        public void fnt_agregar(string idEstudiante, DateTime fecha, float subtotal, float iva, float total)
+        {
+            try
+            {
+                cls_conexion objConecta = new cls_conexion();
+                SqlCommand con = new SqlCommand("sp_registrarmatricula", objConecta.connection);
+                con.CommandType = CommandType.StoredProcedure;
+                con.Parameters.AddWithValue("@fkid_tbl_estudiantes", idEstudiante);
+                con.Parameters.AddWithValue("@fecha", fecha);
+                con.Parameters.AddWithValue("@subtotal", subtotal);
+                con.Parameters.AddWithValue("@iva", iva);
+                con.Parameters.AddWithValue("@total", total);
+                objConecta.connection.Open();
+                con.ExecuteNonQuery();
+                objConecta.connection.Close();
+                str_mensaje = "Registro de matrícula exitoso";
+            }
+            catch (Exception ex)
+            {
+                str_mensaje = "Error al registrar matrícula: " + ex.Message;
+            }
+        }
+
+        public string getMensaje() { return this.str_mensaje; }
+
+        public string getIdEstudiante() { return this.str_idestudiante;}
+
+        public DateTime getFecha() { return this.dt_fecha; }
+
+        public float getSubtotal() { return this.flt_subtotal; }
+
+        public float getIva() { return this.flt_iva;}
+
+        public float getTotal() { return this.flt_total;}
+    }
+
+}
---
+++ Sistema Academico/frm_Matricula.aspx.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Sistema_Academico
+{
+    public partial class frm_Matricula : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btn_guardar_Click(object sender, EventArgs e)
+        {
+            cls_matricula objMatricula = new cls_matricula();
+
+            string idEstudiante = txt_idestudiante.Text; 
+            DateTime fecha = DateTime.Parse(txt_fecha.Text); 
+            float subtotal = float.Parse(txt_subtotal.Text); 
+            float iva = float.Parse(txt_iva.Text); 
+            float total = float.Parse(txt_total.Text); 
+
+           
+            objMatricula.fnt_agregar(idEstudiante, fecha, subtotal, iva, total);
+
+            lbl_mensaje.Text = objMatricula.getMensaje();
+        }
+
+        protected void btn_guardar_Click1(object sender, EventArgs e)
+        {
+            cls_matricula objMatricula = new cls_matricula();
+
+            string idEstudiante = txt_idestudiante.Text;
+            DateTime fecha = DateTime.Parse(txt_fecha.Text);
+            float subtotal = float.Parse(txt_subtotal.Text);
+            float iva = float.Parse(txt_iva.Text);
+            float total = float.Parse(txt_total.Text);
+
+
+            objMatricula.fnt_agregar(idEstudiante, fecha, subtotal, iva, total);
+
+            lbl_mensaje.Text = objMatricula.getMensaje();
+        }
+    }
+}
